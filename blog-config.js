0e46c5237488f258f076027197614556fce9b50c
dd <12202340@inha.edu>
2023-08-22 18:32:14 +0900

    NongDamGom

--- blog-config.js
+++ blog-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Yelog",
   description: "개발하는 농담곰",
-  author: "YeDamGom",
+  author: "NongDamGom",
   siteUrl: "https://yelog.site/",
   links: {
     github: "https://github.com/Yekim1",
