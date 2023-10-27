1dd10568491e187fa34001431731ed280807ec91
dd <12202340@inha.edu>
2023-10-27 16:03:29 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,6 @@ module.exports = {
         policy:[{userAgent: '*', allow: '/'}],
       },
     },
-    `gatsby-plugin-advanced-sitemap`,
     {
       resolve: `gatsby-plugin-react-redux`,
       options: {
