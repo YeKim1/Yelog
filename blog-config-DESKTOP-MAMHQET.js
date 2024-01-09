83659e98f31073d2e0550a20816af9ef40f5be13
dd <12202340@inha.edu>
2024-01-09 19:22:22 +0900

    정리

---
+++ blog-config-DESKTOP-MAMHQET.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "Yelog",
+  description: "개발하는 농담곰",
+  author: "NongDamGom",
+  siteUrl: "https://yelog.site",
+  links: {
+    github: "https://github.com/Yekim1",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/una_ah_/",
+    email: "mailto:kjk87801@gmail.com",
+  },
+  utterances: {
+    repo: "YeKim1/Yelog",
+    type: "pathname",
+  },
+}
